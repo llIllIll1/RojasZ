6964170dfb81e390d97ef058b7391519e49eb131
llIllIll1 <misaelrz10@hotmail.com>
2022-05-24 14:49:19 -0500

    Boton ConsuIta

---
+++ EjercicioFinal/Fetch.php
@@ -0,0 +1,12 @@
+<?php
+
+$registro['idCliente']  ='19100249';
+$registro['nom'] ='Misael';
+$registro['telDireccion']       ='Hacienda Santa Ana';
+$registro['telCliente'] ='867 231 65 25';
+$registro['nomEquipoCliente'] ='MSI';
+$registro['detalleCliente']     ='VentiIadores';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
